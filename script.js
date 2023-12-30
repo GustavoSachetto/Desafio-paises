7599de6ebfb107424849fab0ecbc6277ceb5f52d
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-12-30 14:17:17 -0300

    add

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     const txtSearch = $("#txtSearch");
     const slcSearch = $("#slcSearch");
+    const content = $("article#content");
     const containerCountries = $(".container-countries");
     const urlDefault = "https://restcountries.com/v3.1/";
-    
+
+    let result;
     let urlComplete;
 
     $(".container-theme").click(() => {
@@ -17,10 +19,18 @@ $(document).ready(function() {
             $(".theme-text").text("Dark Mode");
         }
     });
+
+    async function getData(url) {
+        await $.getJSON(url, (data) => {
+            result = data;
+        });
+        return result;
+    }
     
-    function setCountries(url) {
+    async function setCountries() {
         let countries = "";
-        $.getJSON(url, (result) => {
+
+        await getData(urlComplete).then((result) => {
             result.forEach(element => {
                 let country = `
                 <div id="${element.name['common']}" class="container-country">
@@ -46,17 +56,94 @@ $(document).ready(function() {
                 `;
                 countries += country;
             });
-            containerCountries.html(countries);
+        }).catch(() => {
+            countries = `<p>Country not found!</p>`;
+        });
+        
+        containerCountries.html(countries);
+        
+        $(".container-country").click((e) => {
+            setCountryDetails(e.currentTarget.id);
+        });   
+    }
+
+    function setCountryDetails(countryName) {
+        prepareUrl(`name/${countryName}`);
+
+        let countryDetails = "";
+        getData(urlComplete).then((result) => {
+            countryDetails = `
+            
+            <section class="container-details">
+                <button id="country-back">
+                    <i class='bx bx-arrow-back'></i>
+                    <span>Back</span>
+                </button>
+                <div id="${result[0].name['common']}" class="country-datails">
+                    <img src="${result[0].flags['svg']}" alt="${result[0].name['common']}-img" class="country-img">
+                    <div class="country-text">
+                        <h2 class="country-name">${result[0].name['common']}</h2>
+                        <ul class="country-information">
+                            <li>
+                                <strong>Native Name:</strong>
+                                <span>${result[0].name['official']}</span>
+                            </li>
+                            <li>
+                                <strong>Population:</strong>
+                                <span>${result[0]['population']}</span>
+                            </li>
+                            <li>
+                                <strong>Region:</strong>
+                                <span>${result[0]['region']}</span>
+                            </li>
+                            <li>
+                                <strong>Sub Region:</strong>
+                                <span>${result[0]['subregion']}</span>
+                            </li>
+                            <li>
+                                <strong>Capital:</strong>
+                                <span>${result[0]['capital']}</span>
+                            </li>
+                            <li>
+                                <strong>Top Level Domain:</strong>
+                                <span>${result[0]['tld']}</span>
+                            </li>
+                            <li>
+                                <strong>Currencies:</strong>
+                                <span>${result[0].currencies.toString()}</span>
+                            </li>
+                            <li>
+                                <strong>Languages:</strong>
+                                <span>${result[0].currencies['name']}</span>
+                            </li>
+                        </ul>
+                        <ul class="country-border">
+                            <li>
+                                <strong>Border Countries:</strong>
+                                ${ "A"}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            `;
+            
+            content.html(countryDetails);
         });
     }
 
     function prepareUrl(complement) {
         urlComplete = urlDefault + (complement == null ? "all" : complement); 
-        setCountries(urlComplete);       
     }
 
     function filterSearch(target) {
-        target == "txtSearch" ? prepareUrl(`name/${txtSearch.val()}`) : prepareUrl(`region/${slcSearch.val()}`);
+        if (target == "txtSearch") {
+            txtSearch.val() != "" ? prepareUrl(`name/${txtSearch.val()}`) : prepareUrl();
+        } else {
+            slcSearch.val() != "" ? prepareUrl(`region/${slcSearch.val()}`) : prepareUrl();
+        }
+
+        setCountries();
     }
 
     $(".search").change((e) => {
@@ -64,4 +151,5 @@ $(document).ready(function() {
     });
 
     prepareUrl();
+    setCountries();
 });
